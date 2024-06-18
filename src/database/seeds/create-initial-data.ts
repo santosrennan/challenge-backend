8ac1f8ce6bc8a0b761e9f64f7c359cc88086659f
santosrennan <rennandossantos1@gmail.com>
2024-06-17 21:55:35 -0300

    refact: ajustes nas pastas e config do projeto

--- src/database/seeds/create-initial-data.ts
+++ src/database/seeds/create-initial-data.ts
@@ -1,16 +1,15 @@
 import { DataSource } from 'typeorm';
 import { ContentEntity } from '@infrastructure/persistence/typeorm/content.entity';
 import { ContentType } from '@common/enums/content-type.enum';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { environment } from '@common/config/environment';
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.TYPEORM_HOST,
-  port: +process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+  host: environment.typeorm.host,
+  port: environment.typeorm.port,
+  username: environment.typeorm.username,
+  password: environment.typeorm.password,
+  database: environment.typeorm.database,
   entities: [ContentEntity],
 });
 
